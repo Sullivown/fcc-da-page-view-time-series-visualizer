714cd8c800aed5a69ce47bc1b095cb403bcf15d5
Daniel Sullivan <daniel.w.t.sullivan@gmail.com>
2024-06-04 12:13:59 +0000

    fixed datetime import issue

--- time_series_visualizer.py
+++ time_series_visualizer.py
@@ -5,7 +5,7 @@ from pandas.plotting import register_matplotlib_converters
 register_matplotlib_converters()
 
 # Import data (Make sure to parse dates. Consider setting index column to 'date'.)
-df = pd.read_csv('fcc-forum-pageviews.csv', parse_dates=True, index_col='date')
+df = pd.read_csv('fcc-forum-pageviews.csv', parse_dates=['date'], index_col='date')
 
 # Clean data
 df = df[(df['value'] > df['value'].quantile(0.25)) & (df['value'] < df['value'].quantile(0.975))]
